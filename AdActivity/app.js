888dea515fd0a31f1cc70e30e8a3420e55d07c05
Antonio Tobar <antoniotobar@Antonios-MacBook-Air.local>
2019-07-16 13:19:08 -0500

    DOM

---
+++ AdActivity/app.js
@@ -0,0 +1,17 @@
+
+// call the getRandomAd function
+
+// get the adImage by id
+
+// set the src property to the new random ad image  
+
+let addAd = document.getElementById('adImage')
+
+ addAd.src= getRandomAd() 
+ 
+ addAd.style.backgroundColor = "black"
+ addAd.style.border = "solid red 20px"
+
+
+
+
